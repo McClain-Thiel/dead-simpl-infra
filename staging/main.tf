a298c491895fbdd8a218d3904c2e71070d9666f2
McClain Thiel <Mcclain.thiel@gmail.com>
2025-09-07 00:15:54 +0100

    Ignore Terraform artifacts and commit clean changes

--- staging/main.tf
+++ staging/main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
   }
 }
 
@@ -20,12 +24,29 @@ provider "google" {
   region  = var.region
 }
 
+# Enable required APIs
+resource "google_project_service" "servicenetworking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "sqladmin" {
+  project = var.project_id
+  service = "sqladmin.googleapis.com"
+}
+
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.staging_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.staging_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+# Get existing cluster info for IP restrictions  
+data "google_container_cluster" "staging_cluster_info" {
+  name     = "staging-cluster"
+  location = var.region
 }
 
 resource "google_container_cluster" "staging_cluster" {
@@ -80,4 +101,99 @@ resource "google_compute_global_address" "frontend_staging_ip" {
 
 resource "google_compute_global_address" "backend_staging_ip" {
   name = "backend-staging-ip"
+}
+
+# Enable private services access for CloudSQL
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "private-ip-alloc"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+
+# CloudSQL Postgres instance for staging
+resource "google_sql_database_instance" "staging_postgres" {
+  name             = "staging-postgres"
+  database_version = "POSTGRES_15"
+  region           = var.region
+  deletion_protection = false
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc.id
+      enable_private_path_for_google_cloud_services = true
+    }
+    
+    backup_configuration {
+      enabled = true
+      start_time = "03:00"
+    }
+  }
+}
+
+
+# Database and user
+resource "google_sql_database" "staging_db" {
+  name     = "staging_db"
+  instance = google_sql_database_instance.staging_postgres.name
+}
+
+resource "google_sql_user" "staging_user" {
+  name     = "staging_user"
+  instance = google_sql_database_instance.staging_postgres.name
+  password = random_password.db_password.result
+}
+
+resource "random_password" "db_password" {
+  length  = 16
+  special = true
+}
+
+# Store database credentials in Secret Manager
+resource "google_secret_manager_secret" "staging_db_url" {
+  secret_id = "staging-db-url"
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "staging_db_url" {
+  secret = google_secret_manager_secret.staging_db_url.id
+  secret_data = "postgresql://${google_sql_user.staging_user.name}:${random_password.db_password.result}@${google_sql_database_instance.staging_postgres.private_ip_address}/${google_sql_database.staging_db.name}"
+}
+
+# Create database URL secret in Kubernetes
+data "google_secret_manager_secret_version" "staging_db_url" {
+  secret = google_secret_manager_secret.staging_db_url.secret_id
+  depends_on = [google_secret_manager_secret_version.staging_db_url]
+}
+
+resource "kubernetes_secret" "staging_db_url" {
+  metadata {
+    name      = "staging-db-url"
+    namespace = "staging"
+  }
+
+  data = {
+    "SUPABASE_DB_STRING" = data.google_secret_manager_secret_version.staging_db_url.secret_data
+  }
+
+  type = "Opaque"
 }
